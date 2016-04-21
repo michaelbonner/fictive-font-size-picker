6748a617cf5ea3ca3174bd805cdaef187d6d2a7f
Michael Bonner <michaelwbonner@gmail.com>
2016-04-20 21:18:03 -0600

    inital commit

---
+++ fictive-font-size-picker.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Fictive Font Size Picker
+Plugin URI: https://github.com/shekki25/fictive-font-size-picker
+Description: Simple widget to control font size on page
+Author: Fictive Web
+Version: 1.0
+Author URI: https://fictiveweb.com/
+Text Domain: fictive-font-size-picker
+License: GPLv3
+*/
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
---
+++ readme.txt
@@ -0,0 +1,7 @@
+Author: Michael Bonner
+Requires at least: 4.2
+Tested up to: 4.5
+Stable tag: 1.0
+License: GPLv3
+
+Simple widget to control font size on page
